464bf505329933fdb345d0bd6480bb69c8ff7c15
GUTI006 <158616867+GUTI006@users.noreply.github.com>
2025-02-13 23:40:33 +0100

    Update restaurante.py

--- restaurante.py
+++ restaurante.py
@@ -1,31 +1,34 @@
-def calcular_descuento(monto):
-    if monto >= 200000:
-        descuento = monto * 0.15
-    elif monto >= 50000:
-        descuento = monto * 0.02
-    elif monto >= 20000:
-        descuento = monto * 0.015
+def calcularDescuento(valorCompra):
+    if valorCompra >= 200000:
+        descuento = valorCompra * 0.15
+    elif valorCompra >= 50000:
+        descuento = valorCompra * 0.02
+    elif valorCompra >= 20000:
+        descuento = valorCompra * 0.015
     else:
         descuento = 0
     return descuento
 
-def imprimir_factura(nombre_cliente, monto_compra):
-    descuento = calcular_descuento(monto_compra)
-    total_a_pagar = monto_compra - descuento
+def imprimirFactura(nombreCliente, valorDeCompra):
+    descuento = calcularDescuento(valorDeCompra)
+    totalPagar = valorDeCompra - descuento
     
     print("\n--- FACTURA DE COBRO ---")
-    print(f"Nombre del cliente: {nombre_cliente}")
-    print(f"Monto de la compra: ${monto_compra:,.2f}")
+    print(f"Nombre del cliente: {nombreCliente}")
+    print(f"Monto de la compra: ${valorDeCompra:,.2f}")
     print(f"Valor del descuento: ${descuento:,.2f}")
-    print(f"Total a pagar: ${total_a_pagar:,.2f}")
+    print(f"Total a pagar: ${totalPagar:,.2f}")
     print("-------------------------")
     print("¡Gracias por su compra!")
 
 def main():
-    nombre_cliente = input("Ingrese su nombre: ")
-    monto_compra = float(input("Ingrese el monto de la compra: $"))
+    nombreCliente = input("Ingrese su nombre: ")
+    valorDeCompra = float(input("Ingrese el monto de la compra: $"))
     
-    imprimir_factura(nombre_cliente, monto_compra)
+    imprimirFactura(nombreCliente, valorDeCompra)
+
+if __name__ == "__main__":
+    main()
 
 if __name__ == "__main__":
     main()
